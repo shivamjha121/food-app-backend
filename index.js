2935f88e947ab173dcf7c3fd57d40046f6ab4447
Shivam Jha <83417416+shivamjha121@users.noreply.github.com>
2023-03-29 01:05:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://grand-trifle-c397e8.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
